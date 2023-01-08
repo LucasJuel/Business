368e76644d39d98e9b92e615a2cb9ceca7bf6f29
LucasJuel <lucas.juel.sjostrom@gmail.com>
2023-01-08 02:32:42 +0100

    add About me

--- afklar-clean/php/mail_service.php
+++ afklar-clean/php/mail_service.php
@@ -2,7 +2,19 @@
 include './db_connection.php';
 $conn = OpenCon();
 
-echo $_GET['name'];
+echo 'Tak for registreringen ', $_POST['surname'];
+
+$navn = $_POST['name'];
+$efternavn = $_POST['surname'];
+$tel = $_POST['phone'];
+$mail = $_POST['mail'];
+$cvr = $_POST['cvr'];
+$pakke = $_POST['pakke'];
+$desc = $_POST['desc'];
+
+$besked = "Der er en ny henvendelse fra $navn $efternavn\n Information:\n Telefon: $tel\n Mail: $mail\n CVR-nummer: $cvr\n Pakke: $pakke\n Og har inkluderet beskeden: $desc";
+
+mail('pernille.sjostrom@gmail.com', 'Ny henvendelse nr: *indsæt senere*', $besked);
 
 CloseCon($conn);
 ?>
